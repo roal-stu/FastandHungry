89e22cfe98f62529bdc322b4910de27d3c31daa0
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-04-21 00:36:02 -0400

    Adding a view for edit an element

--- fastAndHungry/menu/views.py
+++ fastAndHungry/menu/views.py
@@ -24,13 +24,13 @@ class ElementView(View):
 class CreateElementView(View):
     """Create new artist
     """
-
     template = "menu/add_element.html"
 
     def get(self, request):
         """GET method."""
         form = ElementForm()
-        context = {"form": form}
+        title = 'Agregar platillo'
+        context = {"form": form, "title" : title}
         return render(request, self.template, context)
     
     def post(self, request):
@@ -38,7 +38,8 @@ class CreateElementView(View):
         form = ElementForm(request.POST, request.FILES)
         context = {}
         if not form.is_valid():
-            context = {"form": form}
+            title = 'Agregar platillo'
+            context = {"form": form, "title" : title}
             return render(request, self.template, context)
 
         name = form.cleaned_data.get("name") 
@@ -52,5 +53,36 @@ class CreateElementView(View):
                              image = img 
                                  ) 
         obj.save() 
+        return redirect("/")
+
+class EditElementView(View):
+    """Create new artist
+    """
+    template = "menu/add_element.html"
+
+    def get(self, request, id):
+        """GET method."""
+        title = 'Editar platillo'
+        element = get_object_or_404(Element, id=id)
+        form = ElementForm(initial={'name': element.name, 'price': element.price, 
+                                    'description': element.description, 'image': element.image } )
+        context = {"form": form, "title" : title}
+        return render(request, self.template, context)
+    
+    def post(self, request, id):
+        """Receive and validate sign up form."""
+        element = get_object_or_404(Element, id=id)
+        form = ElementForm(request.POST, request.FILES)
+        context = {}
+        if not form.is_valid():
+            title = 'Editar platillo'
+            context = {"form": form, "title" : title}
+            return render(request, self.template, context)
+
+        element.name = form.cleaned_data.get("name") 
+        element.price = form.cleaned_data.get("price")
+        element.description = form.cleaned_data.get("description")
+        element.img = form.cleaned_data.get("image") 
+        element.save() 
         return redirect("/")
 
