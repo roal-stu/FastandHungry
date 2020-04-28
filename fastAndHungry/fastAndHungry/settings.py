4f4fba190e46ae14f33803062aa644feea7e34f8
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-04-27 22:12:07 -0400

    Estilo provisional

--- fastAndHungry/fastAndHungry/settings.py
+++ fastAndHungry/fastAndHungry/settings.py
@@ -128,4 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
+LOGIN_URL = "/login"
