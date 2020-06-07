4b3d47158f0c4611543542f1e90144b754ce3a7b
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-06-07 19:21:55 -0400

    Beginning second iteration, adding details regarding the first

--- fastAndHungry/users/models.py
+++ fastAndHungry/users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
 	is_customer = models.BooleanField(default=False)
 	is_admin = models.BooleanField(default=False)
 	is_delivery_man = models.BooleanField(default=False)
-	
+
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
 
