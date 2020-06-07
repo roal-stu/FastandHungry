292a2318c407021a532cce4a3b1f78b036a76fed
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-06-07 19:41:24 -0400

    show all the elements of only one category of the menu

--- fastAndHungry/restaurante/urls.py
+++ fastAndHungry/restaurante/urls.py
@@ -8,9 +8,11 @@ from django.conf import settings
 # Views
 from restaurante import views
 
+app_name = "restaurante"
 urlpatterns = [
 	path('', views.Index.as_view(), name='index'),
 	path('menu/', views.Menu.as_view(), name='menu'),
+	path('menu/category/<int:pk>/', views.CategoryView.as_view(), name='category'),
 
 	path('elements-admin/', views.Elements.as_view(), name = 'elements_admin'),
 	path('update-element/<int:pk>/', views.ElementUpdate.as_view(), name="element_edit"),
