a15fdcdca464dcd6ac042592507d2321de5ec5ea
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-06-08 20:13:06 -0400

    adding models for orders

--- fastAndHungry/restaurante/models.py
+++ fastAndHungry/restaurante/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import *
+
 # Create your models here.
 
 
@@ -30,14 +32,13 @@ class Element(models.Model):
     TODO: represents a element.
     Each element must be related to a category
     """
-
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     description = models.CharField(max_length=280)
     image = models.ImageField(upload_to=element_image_directory_path, null=True, blank=True)
 
     #relatiionship
-    category = models.ForeignKey(Category,models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
 
     def __str__(self):
         """Get str representation."""
@@ -46,3 +47,35 @@ class Element(models.Model):
     def __repr__(self):
         """Get str representation."""
         return self.__str__()
+
+
+class Order(models.Model):
+    """Order Model.
+    TODO: represents an order.
+    """
+    ORDER_STATES = [
+        ('CT','CARRITO'),
+        ('PD','PENDIENTE'),
+        ('LT','LISTA'),
+        ('EC','EN CAMINO'),
+        ('ET','ENTREGADO'),
+    ]
+
+    state = models.CharField(choices=ORDER_STATES,max_length=2)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address = models.ForeignKey(Address,null=True,on_delete=models.SET_NULL)
+
+
+class OrderElement(models.Model):
+    """Represents a element with a quantity.
+    TODO: represents a element inside a order
+    """
+    element = models.ForeignKey(Element,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def get_subtotal(self):
+        return self.quantity * self.element.price
+
+    def __str__(self):
+        return self.quantity 
