40c88018e96a745b50641072a221e34217e72943
Ramón Arenas <ramon_cmk@ciencias.unam.mx>
2020-06-14 01:54:23 -0400

    restructions in direction

--- fastAndHungry/restaurante/forms.py
+++ fastAndHungry/restaurante/forms.py
@@ -6,3 +6,14 @@ from .models import*
 
 class AddToCartForm(forms.Form):
     quantity = forms.IntegerField(min_value=1)
+
+class MakeAnOrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['address']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(MakeAnOrderForm, self).__init__(*args, **kwargs)
+        if self.request: 
+            self.fields['address'].queryset = self.fields['address'].queryset.filter(usuario=self.request.user)
